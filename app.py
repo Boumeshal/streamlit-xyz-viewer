19818bf17ca3252486997e87924578d2ea464ffc
Mouaad BOUMESHAL <mouaad.boumeshal@socotec.com>
2025-07-16 16:50:26 +0200

    message clair

--- app.py
+++ app.py
@@ -180,24 +180,25 @@ try:
 except Exception as e:
     st.error(f"❌ Erreur lors de la création du graphique Plotly : {e}")
 
-    # --- AFFICHAGE PLOTLY 2D scatter (valeurs en fonction de l'ordre des points) ---
+# --- AFFICHAGE PLOTLY 2D scattergl (valeurs en fonction de l'ordre des points) ---
 try:
     fig2d = go.Figure(data=[
-        go.Scatter(
+        go.Scattergl(
             x=list(range(len(values))),
             y=values,
             mode="markers+lines",
             marker=dict(color="blue"),
             line=dict(shape="linear"),
-            name="Values Scatter"
+            name="Values ScatterGL"
         )
     ])
     fig2d.update_layout(
-        title="📈 Scatter plot 2D des valeurs (ordre des points)",
+        title="📈 ScatterGL plot 2D des valeurs (ordre des points)",
         xaxis_title="Index du point",
         yaxis_title="Valeur",
+        yaxis=dict(range=[0, 10000]),
         margin=dict(l=40, r=40, t=40, b=40)
     )
     st.plotly_chart(fig2d, use_container_width=True)
 except Exception as e:
-    st.error(f"❌ Erreur lors de la création du graphique 2D scatter : {e}")
+    st.error(f"❌ Erreur lors de la création du graphique 2D scattergl : {e}")
